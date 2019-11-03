68f8d1fcaf783cb2a435816a6fba4182ae5810a5
Hachemi <hachemi@protonmail.com>
2019-11-03 18:49:59 +0100

    Set the button edit to get the correct id and edit the item in the database

--- app.js
+++ app.js
@@ -52,7 +52,7 @@ app.get("/", (req, res) => {
                 <li class="list-group-item list-group-item-action d-flex align-items-center justify-content-between">
                   <span class="item-text">${item.dataValues.item}</span>
                   <div>
-                    <button class="edit-me btn btn-secondary btn-sm mr-1">Edit</button>
+                    <button data-id="${item.dataValues.id}" class="edit-me btn btn-secondary btn-sm mr-1">Edit</button>
                     <button class="delete-me btn btn-danger btn-sm">Delete</button>
                   </div>
                 </li>`;
@@ -82,8 +82,14 @@ app.post("/", (req, res) => {
 });
 
 app.post("/update-item", (req, res) => {
-  console.log(req.body.itemUpdated);
-  res.send("Success");
+  Item.update(
+    { item: req.body.itemUpdated },
+    {
+      where: {
+        id: req.body.id
+      }
+    }
+  );
 });
 
 app.listen(port);
--- public/js/edit.js
+++ public/js/edit.js
@@ -2,7 +2,10 @@ document.addEventListener("click", function(e) {
   if (e.target.classList.contains("edit-me")) {
     let userInput = prompt("Veuillez entrer votre nouvelle tâche ...");
     axios
-      .post("/update-item", { itemUpdated: userInput })
+      .post("/update-item", {
+        itemUpdated: userInput,
+        id: e.target.getAttribute("data-id")
+      })
       .then()
       .catch(err => {
         console.log(err);
